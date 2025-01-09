c7cc1ad7b5b41264ce7ff0b5ec8d568055c35dba
joemens25 <alvinjoemens@gmail.com>
2025-01-09 10:19:27 +0000

    initial commit

--- script.js
+++ script.js
@@ -2,15 +2,4 @@ function showCart() {
 alert("show item");
 }
 const button = document.getElementById("searchButton");
-button.addEventListener("click", searchButton);
-
-const showCart = document.getElementById("cartIcon");
-const shoppingCart = document.getElementById(shoppingCart);
-
-showCart.addEventListener("click", () =>{
-    if (shoppingCart.style.display === "none" || shoppingCart.style.display ==="") {
-        shoppingCart.style.display === "block"; } 
-     else {
-        shoppingCart.style.display = "none"
-     }
-});
+button.addEventListener("click", showCart);
